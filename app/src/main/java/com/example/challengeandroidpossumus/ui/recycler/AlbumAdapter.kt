09b6a10e6585c01218e888324ee737d961ad9af9
Franco <franco.salvatierradvp@gmail.com>
2022-07-22 02:04:03 -0300

    Se crea lógica para RecyclerView de albumes

--- app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/AlbumAdapter.kt
+++ app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/AlbumAdapter.kt
@@ -1,6 +1,22 @@
 package com.example.challengeandroidpossumus.ui.recycler
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.challengeandroidpossumus.R
+import com.example.challengeandroidpossumus.data.model.Album
 
-class AlbumAdapter(): RecyclerView.Adapter<>() {
+class AlbumAdapter(private var listAlbumes: List<Album>): RecyclerView.Adapter<AlbumViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        var layoutInflater = LayoutInflater.from(parent.context)
+        return AlbumViewHolder(layoutInflater.inflate(R.layout.item_albumes, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        var title = listAlbumes[position].title
+        var userId = listAlbumes[position].userId
+        //holder.bind(title, userId)
+    }
+
+    override fun getItemCount(): Int = listAlbumes.size
 }
--- app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/AlbumViewHolder.kt
+++ app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/AlbumViewHolder.kt
@@ -2,6 +2,13 @@ package com.example.challengeandroidpossumus.ui.recycler
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.challengeandroidpossumus.databinding.ItemAlbumesBinding
 
 class AlbumViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    private var binding = ItemAlbumesBinding.bind(view)
+
+    fun bind(title: String, userId: Int) {
+        binding.tvTitleAlbum.text = title
+        binding.tvUserId.text = userId.toString()
+    }
 }
