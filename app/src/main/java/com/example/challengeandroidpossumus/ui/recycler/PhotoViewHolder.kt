7d1f4afbf68a168c54104563bd0851454332380f
Franco <franco.salvatierradvp@gmail.com>
2022-07-21 23:22:43 -0300

    Se finaliza código para poder abrir los detalles de cada foto en otra activity

--- app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/PhotoViewHolder.kt
+++ app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/PhotoViewHolder.kt
@@ -1,8 +1,11 @@
 package com.example.challengeandroidpossumus.ui.recycler
 
+import android.content.Intent
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.challengeandroidpossumus.databinding.ItemPhotoBinding
+import com.example.challengeandroidpossumus.ui.view.PhotoDetailsActivity
 import com.squareup.picasso.Picasso
 
 class PhotoViewHolder(view: View): RecyclerView.ViewHolder(view) {
@@ -10,5 +13,15 @@ class PhotoViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     fun bind(url: String){
         Picasso.get().load(url).into(binding.ivPhoto)
+    }
+
+    init {
+        view.setOnClickListener{
+            var position = adapterPosition
+            var intent = Intent(it.context, PhotoDetailsActivity::class.java).apply {
+                putExtra("id", position)
+            }
+            ContextCompat.startActivity(it.context, intent, null)
+        }
     }
 }
---
+++ app/src/main/java/com/example/challengeandroidpossumus/ui/view/PhotoDetailsActivity.kt
@@ -0,0 +1,36 @@
+package com.example.challengeandroidpossumus.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.challengeandroidpossumus.R
+import com.example.challengeandroidpossumus.data.model.PhotoProvider
+import com.example.challengeandroidpossumus.databinding.ActivityPhotoDetailsBinding
+import com.squareup.picasso.Picasso
+
+class PhotoDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPhotoDetailsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_details)
+
+        binding = ActivityPhotoDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var id = getExtras()
+        initUI(id)
+    }
+
+    private fun getExtras(): Int{
+        var bundle = intent.extras
+        var number = bundle!!.getInt("id")
+
+        return number
+    }
+
+    private fun initUI(id: Int) {
+        binding.tvAlbumId.text = PhotoProvider.photos[id].albumId.toString()
+        binding.tvTitle.text = PhotoProvider.photos[id].title
+        Picasso.get().load(PhotoProvider.photos[id].url).into(binding.ivUrlPhoto)
+    }
+}
