0438d7570a1ad959d87913c282cd91a36d07939a
Franco <franco.salvatierradvp@gmail.com>
2022-07-21 21:03:33 -0300

    Creacion de Adapter y ViewHolder para implementar la lógica de recyclerView

---
+++ app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/PhotoAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.challengeandroidpossumus.ui.recycler
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoAdapter(): RecyclerView.Adapter<PhotoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/challengeandroidpossumus/ui/recycler/PhotoViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.challengeandroidpossumus.ui.recycler
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoViewHolder(view: View): RecyclerView.ViewHolder(view) {
+}
